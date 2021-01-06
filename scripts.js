6b362fd98aedfe22989d7e2ed2efca194bd40679
Carlos Fisac Ferrández <54982406+pyrexBAUS@users.noreply.github.com>
2021-01-06 18:47:45 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function excuseGenerator(){
+
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+    var excuse;
+
+    excuse = who[Math.floor((Math.random() * who.length) )] + " " + action[Math.floor((Math.random() * action.length))] + 
+    " " + what[Math.floor((Math.random() * what.length) )] + " " + when[Math.floor((Math.random() * when.length))]+".";
+
+    document.getElementById("excuse").innerHTML=excuse;
+}
