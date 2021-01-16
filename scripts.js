d95ed6db1307061e58be890653c16549adf8a4d6
Carlos Fisac Ferrández <charles8fisac@gmail.com>
2021-01-16 18:58:04 +0000

    fixed multiple math.random calls

--- scripts.js
+++ scripts.js
@@ -4,10 +4,17 @@ function excuseGenerator(){
     let action = ['ate','peed','crushed','broke'];
     let what = ['my homework', 'the keys', 'the car'];
     let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-    var excuse;
+    let fullSentence = [who, action, what, when]
+    let excuse="";
 
-    excuse = who[Math.floor((Math.random() * who.length) )] + " " + action[Math.floor((Math.random() * action.length))] + 
-    " " + what[Math.floor((Math.random() * what.length) )] + " " + when[Math.floor((Math.random() * when.length))]+".";
+    for(var i=0;i<fullSentence.length,i++){
+        excuse.concat(fullSentence[i][Math.floor((Math.random() * fullSentence[i].length]);
+        excuse.concat(" ");
+    }
+    excuse.concat(".")
+
+    // excuse = who[Math.floor((Math.random() * who.length) )] + " " + action[Math.floor((Math.random() * action.length))] + 
+    // " " + what[Math.floor((Math.random() * what.length) )] + " " + when[Math.floor((Math.random() * when.length))]+".";
 
     document.getElementById("excuse").innerHTML=excuse;
 }
